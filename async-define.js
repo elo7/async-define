19e8d0f1a0c1ebb05faa9f461b58821ac89e1d8f
Luiz Corte Real <login+luiz@elo7.com>
2018-01-22 13:31:06 -0200

    Remove debug & uniform code style

--- async-define.js
+++ async-define.js
@@ -27,14 +27,14 @@
 
     Advantages:
 
-    - Very small (~400 bytes w/ gzip and no debug) so you can inline it on every page.
+    - Very small (~400 bytes w/ gzip) so you can inline it on every page.
     - Don't expect your modules to be loaded in a specific order.
     - Allows asynchronous loading of your files for maximum performance.
     - Very simple.
 
  */
-(function(debug) {
-    if(typeof define == 'undefined') {
+(function() {
+    if (typeof define == 'undefined') {
         // object with all executes modules (module_name => module_value)
         var modules = {};
 
@@ -64,38 +64,32 @@
                 config_dependencies_index = -1;
 
             // config dependecies
-            if(_define.prototype.config_dependencies && _define.prototype.config_dependencies.constructor === Array) {
+            if (_define.prototype.config_dependencies && _define.prototype.config_dependencies.constructor === Array) {
                 var config_dependencies = _define.prototype.config_dependencies || [];
 
                 var config_dependencies_size = config_dependencies.length;
-                for(; config_dependencies_iterator < config_dependencies_size; config_dependencies_iterator++) {
-                    if(name == config_dependencies[config_dependencies_iterator]) {
+                for (; config_dependencies_iterator < config_dependencies_size; config_dependencies_iterator++) {
+                    if (name === config_dependencies[config_dependencies_iterator]) {
                         config_dependencies_index = config_dependencies_iterator;
                     }
                 }
-                if(config_dependencies_index != -1) {
+                if (config_dependencies_index !== -1) {
                     config_dependencies.splice(config_dependencies_index, 1)
                 } else {
                     dependencies = dependencies.concat(config_dependencies);
                 }
             }
-            debug && console.log('registering', name);
 
             // find params
             for (; dependencies_iterator < dependencies.length; dependencies_iterator++) {
                 dependency_name = dependencies[dependencies_iterator];
-                debug && console.log('dependency found on', dependency_name);
 
                 // if this dependency exists, push it to param injection
                 if (modules.hasOwnProperty(dependency_name)) {
-                    debug && console.log('dependency is already loaded; continuing');
                     params.push(modules[dependency_name]);
                 } else if (dependency_name === 'exports') {
-                    debug && console.log('pushing exports');
                     params.push(exports);
                 } else {
-                    debug && console.log('dependency not loaded; waiting on it');
-
                     if (argc != 4) { // if 4 values, is reexecuting
                         // no module found. save these arguments for future execution.
                         define_queue[dependency_name] = define_queue[dependency_name] || [];
@@ -108,18 +102,14 @@
 
             // all dependencies are satisfied, so proceed
             if (dependencies_satisfied) {
-                debug && console.log('all dependencies for', name, 'were satisfied; instantiating');
-
-                if(!modules.hasOwnProperty(name)) {
+                if (!modules.hasOwnProperty(name)) {
                     // execute this module
                     result = factory.apply(this, params);
 
                     if (result) {
-                        debug && console.log('module returned value');
                         modules[name] = result;
                     } else {
                         // assuming result is in exports object
-                        debug && console.log('module did not return value; assuming exports was used');
                         modules[name] = exports;
                     }
                 }
@@ -144,4 +134,4 @@
         define = _define;
         undefine = _undefine;
     }
-})(false);
+})();
